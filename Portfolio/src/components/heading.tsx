915eb33dc308c1aed68b1c7ae8331efe3db5d6e6
Artur Jordão <artur.dev@hotmail.com>
2023-06-01 13:42:33 -0300

    Update heading.tsx

--- Portfolio/src/components/heading.tsx
+++ Portfolio/src/components/heading.tsx
@@ -1,80 +1,86 @@
-import { faGithub, faLinkedin, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
-import { faFile } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGithub,
+	faLinkedin,
+	faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Col, Container, Row } from 'react-bootstrap'
-import '../css/heading.css'
 import head from '../assets/head.svg'
-
+import '../css/heading.css'
 
 export function Heading() {
 	return (
-		<div id='head'>
-		<div className="heading">
-			<Container fluid className="text-center">
-				<Row className="d-flex align-items-center justify-content-center">
-					<Col md={6}>
-						<div id="heading">
-							<h1 id="intro"	>
-								Olá, sou <br /> <span className="nome">Artur Jordão</span>{' '}
-								<br />
-								Web Developer
-							</h1>
-							<div className="wrapper">
-								<a
-									className="linkedin icon"
-									href="https://www.linkedin.com/in/artur-jord%C3%A3o-1340b0221/"
-									role="button"
-									id="linkedinBtn"
-									target="_blank"
-								>
-									<div className="tooltip">LinkedIn</div>
-									<FontAwesomeIcon
-										className="icon-container"	
-										icon={faLinkedin}
-									/>
-								</a>
-								<a
-									className="github icon"
-									href="https://github.com/ArturJordaoo"
-									role="button"
-									id="gitBtn"
-									target="_blank"
-								>
-									<div className="tooltip">GitHub</div>
-									<FontAwesomeIcon className="icon-container" icon={faGithub} />
-								</a>
-								<a
-									className="whatsapp icon"
-									href="https://wa.me/5581982597573"
-									role="button"
-									id="whatsBtn"
-									target="_blank"
-								>
-									<div className="tooltip">WhatsApp</div>
-									<FontAwesomeIcon className="icon-container" icon={faWhatsapp} />
-								</a>
-								
+		<div id="head">
+			<div className="heading">
+				<Container fluid className="text-center">
+					<Row className="d-flex align-items-center justify-content-center">
+						<Col xs={12} sm={12} md={6}>
+							<div id="heading">
+								<h1 id="intro">
+									Olá, sou <br /> <span className="nome">Artur Jordão</span>{' '}
+									<br />
+									Web Developer
+								</h1>
+								<div className="wrapper">
+									<a
+										className="linkedin icon"
+										href="https://www.linkedin.com/in/artur-jord%C3%A3o-1340b0221/"
+										role="button"
+										id="linkedinBtn"
+										target="_blank"
+									>
+										<div className="tooltip">LinkedIn</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faLinkedin}
+										/>
+									</a>
+									<a
+										className="github icon"
+										href="https://github.com/ArturJordaoo"
+										role="button"
+										id="gitBtn"
+										target="_blank"
+									>
+										<div className="tooltip">GitHub</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faGithub}
+										/>
+									</a>
+									<a
+										className="whatsapp icon"
+										href="https://wa.me/5581982597573"
+										role="button"
+										id="whatsBtn"
+										target="_blank"
+									>
+										<div className="tooltip">WhatsApp</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faWhatsapp}
+										/>
+									</a>
+								</div>
+								<div className="curriculo">
+									<button className="btn btn-light">
+										<FontAwesomeIcon className="icon-container" icon={faFile} />
+										<span> CV DOWNLOAD </span>
+									</button>
+								</div>
 							</div>
-							<div className='curriculo'><button className='btn btn-light'>
-							<FontAwesomeIcon className='icon-container' icon= {faFile}/>
-								<span> CV DOWNLOAD  </span>
-								</button></div>
-							
-						</div>
-					</Col>
-						<Col md={6} className='profile' >
-						<div className='propic'>
-							<img
-								src={head}
-								alt="profile,pic"
-								
-							/>
-						</div>
-						
-					</Col>
-				</Row>
-			</Container>
-		</div>
+						</Col>
+						<Col xs={12} sm={12} md={6}>
+							<div className="profile">
+								<div className="propic">
+									<img src={head} alt="profile,pic" />
+								</div>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		</div>
 	)
 }
