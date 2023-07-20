be031f0bec68c966b8cd52d16f378135c4ad4187
Artur Jordão <artur.dev@hotmail.com>
2023-07-20 01:03:58 -0300

    a

--- Portfolio/src/components/Projetos.tsx
+++ Portfolio/src/components/Projetos.tsx
@@ -1,53 +1,64 @@
 import {
 	Box,
 	Button,
-	Center,
-	Flex,
 	Grid,
-	GridItem,
 	Heading,
 	Image,
-	Link,
 	Text,
 	Wrap,
 	WrapItem,
 } from '@chakra-ui/react'
+import React from 'react'
+
+interface ProjetoProps {
+	nome: string
+	habilidades: string[]
+	descricao: string
+	imagem: string
+	link: string
+}
 
 interface ProjetosProps {
 	habilidades: string[]
 }
-interface ProjetosProps {
-	habilidades: string[]
-}
+
 export function Projetos({ habilidades }: ProjetosProps) {
 	// Lista de projetos fictícios
-	const projetos = [
+	const projetos: ProjetoProps[] = [
 		{
 			nome: 'CRUD',
 			habilidades: ['HTML', 'CSS'],
 			descricao: 'Um projeto usando HTML e CSS',
+			imagem: 'CRUDIMG',
 			link: 'https://outlook.live.com/mail/0/inbox/id/AQQkADAwATY0MDABLTBlYTYtOWU2OC0wMAItMDAKABAAulonNU122kKHdsJlc5a1iA%3D%3D/sxs/AQMkADAwATY0MDABLTBlYTYtOWU2OC0wMAItMDAKAEYAAAM1fjP5TP9bT4rJYAuSQX3zBwBS5vr8S7zaT4ttPGxsAJNmawAAAgEJAAAAUub6%2FEu82k%2BLbTxsbACTZmsAAADhsj6jAAAAARIAEADNYq2v6QMsSowPp2awnieA?actSwt=true',
 		},
 		{
 			nome: 'TO DO LIST',
 			habilidades: ['HTML', 'CSS', 'JS'],
 			descricao: 'Aplicação simples utilizando InternalStorage',
-			link: '',
+			imagem: 'TODOLISTIMG',
+			link: 'https://arturjordaoo.github.io/ToDoApp/',
 		},
 		{
 			nome: 'teste',
 			habilidades: ['HTML', 'CSS', 'JS'],
 			descricao: 'Um projeto usando HTML, CSS e JavaScript',
+			imagem: 'TESTEIMG',
 			link: '',
 		},
 		// Adicione mais projetos fictícios conforme necessário
 	]
 
-	// Mapear as habilidades para as imagens correspondentes
 	const habilidadesImagens: Record<string, string> = {
-		HTML: '../../public/images//html.png',
-		CSS: '../../public/images//css-3.png',
-		JS: '../../public/images//js.png',
+		HTML: '../../public/images/html.png',
+		CSS: '../../public/images/css-3.png',
+		JS: '../../public/images/js.png',
+	}
+
+	const projetosImagens: Record<string, string> = {
+		CRUDIMG: '../../public/images/Crud_project.png',
+		TODOLISTIMG: '../../public/images/Todoimg.png',
+		TESTEIMG: '../../public/images/Test_project.png',
 	}
 
 	// Filtrar os projetos com base nas habilidades selecionadas
@@ -56,7 +67,7 @@ export function Projetos({ habilidades }: ProjetosProps) {
 				habilidades.some((habilidade) =>
 					projeto.habilidades.includes(habilidade),
 				),
-      )
+			)
 		: projetos
 
 	return (
@@ -81,6 +92,13 @@ export function Projetos({ habilidades }: ProjetosProps) {
 						<Heading as="h3" size="md" mb="1rem">
 							{projeto.nome}
 						</Heading>
+						<Image
+							src={projetosImagens[projeto.imagem]}
+							alt={projeto.nome}
+							className="imagensProjeto"
+							p="1rem"
+							
+						/>
 						<Button
 							as="a"
 							href={projeto.link}
@@ -88,6 +106,7 @@ export function Projetos({ habilidades }: ProjetosProps) {
 							rel="noopener noreferrer"
 							colorScheme="teal"
 							mb="1rem"
+							isDisabled={!projeto.link}
 						>
 							acesse aqui
 						</Button>
@@ -99,7 +118,7 @@ export function Projetos({ habilidades }: ProjetosProps) {
 										src={habilidadesImagens[habilidade]}
 										alt={habilidade}
 										w="2rem"
-										h="2rem,"
+										h="2rem"
 									/>
 								</WrapItem>
 							))}
