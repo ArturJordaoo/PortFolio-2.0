c76c8b81166a2392eaf3938ad4d6ceb846af35bb
Artur Jordão <artur.dev@hotmail.com>
2023-10-20 20:52:31 -0300

    ok

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,81 +1,92 @@
 import {
-  faGithub,
-  faLinkedin,
-  faWhatsapp,
-} from '@fortawesome/free-brands-svg-icons';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Col, Container, Row } from 'react-bootstrap';
-import head from '../assets/head.svg';
-import '../css/heading.css';
+	faGithub,
+	faLinkedin,
+	faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Col, Container, Row } from 'react-bootstrap'
+import head from '../assets/head.svg'
+import '../css/heading.css'
 
 export function Heading() {
-  return (
-    <div>
-      <div className="heading">
-        <Container fluid className="text-center" id="head">
-          <Row className="d-flex align-items-center justify-content-center">
-            <Col xs={12} sm={12} md={6}>
-              <div id="heading">
-                <h1 id="intro">
-                  Olá, sou <br />{' '}
-                  <span className="nome">Artur Jordão</span> <br />
-                  Desenvolvedor Web
-                </h1>
-                <div className="wrapper">
-                  <a
-                    className="linkedin icon"
-                    href="https://www.linkedin.com/in/artur-jord%C3%A3o-1340b0221/"
-                    role="button"
-                    id="linkedinBtn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="tooltip">LinkedIn</div>
-                    <FontAwesomeIcon className="icon-container" icon={faLinkedin} />
-                  </a>
-                  <a
-                    className="github icon"
-                    href="https://github.com/ArturJordaoo"
-                    role="button"
-                    id="gitBtn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="tooltip">GitHub</div>
-                    <FontAwesomeIcon className="icon-container" icon={faGithub} />
-                  </a>
-                  <a
-                    className="whatsapp icon"
-                    href="https://wa.me/5581982597573"
-                    role="button"
-                    id="whatsBtn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <div className="tooltip">WhatsApp</div>
-                    <FontAwesomeIcon className="icon-container" icon={faWhatsapp} />
-                  </a>
-                </div>
-                <div className="curriculo">
-                  <a className="btn btn-light"
-                  href='../assets/CurriculoArtur(maio).pdf'>
-                    <FontAwesomeIcon className="icon-container" icon={faFile} />
-                    <span> CURRÍCULO </span>
-                  </a>
-                </div>
-              </div>
-            </Col>
-            <Col xs={12} sm={12} md={6}>
-              <div className="profile">
-                <div className="propic">
-                  <img src={head} alt="profile pic" />
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="heading" id="heading">
+				<Container fluid className="text-center" id="head">
+					<Row className="d-flex align-items-center justify-content-center">
+						<Col xs={12} sm={12} md={6}>
+							<div id="heading">
+								<h1 id="intro">
+									Olá, sou <br /> <span className="nome">Artur Jordão</span>{' '}
+									<br />
+									Desenvolvedor Web
+								</h1>
+								<div className="wrapper">
+									<a
+										className="linkedin icon"
+										href="https://www.linkedin.com/in/artur-jord%C3%A3o-1340b0221/"
+										role="button"
+										id="linkedinBtn"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<div className="tooltip">LinkedIn</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faLinkedin}
+										/>
+									</a>
+									<a
+										className="github icon"
+										href="https://github.com/ArturJordaoo"
+										role="button"
+										id="gitBtn"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<div className="tooltip">GitHub</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faGithub}
+										/>
+									</a>
+									<a
+										className="whatsapp icon"
+										href="https://wa.me/5581982597573"
+										role="button"
+										id="whatsBtn"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<div className="tooltip">WhatsApp</div>
+										<FontAwesomeIcon
+											className="icon-container"
+											icon={faWhatsapp}
+										/>
+									</a>
+								</div>
+								<div className="curriculo">
+									<a
+										className="btn btn-light"
+										href="../assets/CurriculoArtur(maio).pdf"
+									>
+										<FontAwesomeIcon className="icon-container" icon={faFile} />
+										<span> CURRÍCULO </span>
+									</a>
+								</div>
+							</div>
+						</Col>
+						<Col xs={12} sm={12} md={6}>
+							<div className="profile">
+								<div className="propic">
+									<img src={head} alt="profile pic" />
+								</div>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		</div>
+	)
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,91 +1,91 @@
-import { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-scroll';
-import '../css/nav.css';
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link } from 'react-scroll'
+import '../css/nav.css'
 
 export function Nav() {
-  const [click, setClick] = useState(false);
+	const [click, setClick] = useState(false)
 
-  const handleClick = () => setClick(!click);
-  const closeMenu = () => setClick(false);
+	const handleClick = () => setClick(!click)
+	const closeMenu = () => setClick(false)
 
-  return (
-    <div className="header">
-      <nav className="navbar">
-        <div className="hamburger" onClick={handleClick}>
-          {click ? (
-            <FaTimes size={30} style={{ color: '#ffffff' }} />
-          ) : (
-            <FaBars size={30} style={{ color: '#ffffff' }} />
-          )}
-        </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="head"
-              spy={true}
-              smooth={true}
-              offset={-200}
-              duration={500}
-              onClick={closeMenu}
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="sobre"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-              onClick={closeMenu}
-            >
-              Sobre
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-              onClick={closeMenu}
-            >
-              Skills
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="project"
-              spy={true}
-              smooth={true}
-              offset={-30}
-              duration={500}
-              onClick={closeMenu}
-            >
-              Projetos
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="x"
-              spy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-              onClick={closeMenu}
-            >
-              Contato
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="header">
+			<nav className="navbar">
+				<div className="hamburger" onClick={handleClick}>
+					{click ? (
+						<FaTimes size={30} style={{ color: '#ffffff' }} />
+					) : (
+						<FaBars size={30} style={{ color: '#ffffff' }} />
+					)}
+				</div>
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<li className="nav-item">
+						<Link
+							activeClass="active"
+							to="heading"
+							spy={true}
+							smooth={true}
+							offset={-60}
+							duration={500}
+							onClick={closeMenu}
+						>
+							Home
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							activeClass="active"
+							to="sobre"
+							spy={true}
+							smooth={true}
+							offset={-60}
+							duration={500}
+							onClick={closeMenu}
+						>
+							Sobre
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							activeClass="active"
+							to="skills"
+							spy={true}
+							smooth={true}
+							offset={-105}
+							duration={500}
+							onClick={closeMenu}
+						>
+							Skills
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							activeClass="active"
+							to="project"
+							spy={true}
+							smooth={true}
+							offset={-65}
+							duration={500}
+							onClick={closeMenu}
+						>
+							Projetos
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							to="x"
+							spy={true}
+							smooth={true}
+							offset={-50}
+							duration={500}
+							onClick={closeMenu}
+						>
+							Contato
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	)
 }
