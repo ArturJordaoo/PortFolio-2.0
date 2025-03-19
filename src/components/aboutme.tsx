47bd318fcbbbf88baa4f7cedf72bfc16fb271c1f
Artur Jordão <artur.dev@hotmail.com>
2025-03-19 20:17:32 -0300

    update

--- src/components/aboutme.tsx
+++ src/components/aboutme.tsx
@@ -18,13 +18,11 @@ export function AboutMe() {
           <Col md={6}>
             <h3 id="webintro">Desenvolvedor Full-stack</h3>
             <p id="abouttxt">
-              Oi, sou Artur, estudante de Ciências da Computação. Desde 2021,
-              minha jornada na programação começou com Python, e hoje sou
-              apaixonado por Front-end. No 6º período, estou em busca de
-              oportunidades nessa área. Durante meu intercâmbio, aprimorei
-              habilidades interculturais e meu inglês.Destaco-me por projetos em
-              React e JavaScript. Estou pronto para desafios no mundo do
-              Front-end!.
+              Oi, sou Artur, estudante de Ciências da Computação. Atualmente no
+              7º período, estou em busca de oportunidades nessa área. Durante
+              meu intercâmbio, aprimorei habilidades interculturais e meu
+              inglês. Destaco-me por projetos em NextJs e NestJs. Estou pronto
+              para desafios no mundo do Full-Stack!.
             </p>
           </Col>
         </Row>
